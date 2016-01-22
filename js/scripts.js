797caaea527b38191539ed05de71ab0906a60c20
vr <student@epicodus.com>
2016-01-22 07:34:16 -0800

    initialized files, added spec to count up to user inputted number, and passed it

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+var countUp = function(input) {
+  var output = [];
+  for (var n=1; n <= input ; n++) {
+    output.push(n);
+  };
+  return output;
+};
+
+
+
+
+
+//CHECK count-up FOR CODE TO TURN ARRAY INTO <ul>
---
+++ spec/specs.js
@@ -0,0 +1,14 @@
+describe("countUp", function() {
+  it("counts up by 1 to the given number", function() {
+    expect(countUp(5)).to.eql([1, 2, 3, 4, 5])
+  });
+});
+
+// describe("replaceThree", function() {
+//   it("replaces every number divisible by 3 with 'ping'")
+//     expect(replaceThree(5)).to.eql([0, 1, 2, ping, ])
+// });
+
+
+
+//CHECK count-up FOR CODE TO TURN ARRAY INTO <ul>
