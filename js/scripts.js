a8a01fc2da1d8601a7b9734b8520fbd4e3227498
vr <student@epicodus.com>
2016-01-22 09:06:21 -0800

    built basic user interface and made code count from 1 to user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
-
-
-
-var countUp = function(input) {   //PASSED
+var countUp = function(countTo) {
   var output = [];
-  for (var n=1; n <= input; n++) {
+  for (var n=0; n <= countTo ; n++) {
     output.push(n);
   };
   return output;
-
 };
+
+function write_letters(endResult){
+    var items = document.getElementById("itemList");
+    for (var i = 0; i < endResult.length; i++ ) {
+        var item = document.createElement("li");
+        item.innerHTML = endResult[i];
+        items.appendChild(item);
+    };
+    return;
+};
+
 
 var replaceThree = function(input) {
   // var pingpong = "";
@@ -18,21 +25,55 @@ var replaceThree = function(input) {
   return "ping";
 };
 
-var replaceFive = function(input) {
-  // var pingpong = "";
-  for(var n = 0; n <= input; n++) {
-  if(n % 5 === 0);
-  };
-  return "pong";
-};
 
-var replaceFifteen = function(input) {
-  // var pingpong = "";
-  for(var n = 0; n <= input; n++) {
-  if(n % 15 === 0);
-  };
-  return "pingpong";
-};
+$(document).ready(function() {
+  $("form#pingpong").submit(function() {
+    //debugger;
+    var countTo = parseInt($("input#countTo").val());
+    var endResult = countUp (countTo);
+    write_letters(endResult);
 
-// var countTo = parseInt($("input#countTo").val());
-//CHECK count-up FOR CODE TO TURN ARRAY INTO <ul>
+    // write_letters(endResult) {
+    //   return items;
+    // }
+
+    //$("#itemList").text(endResult);
+
+    event.preventDefault();
+  });
+});
+// var countUp = function(input) {   //PASSED
+//   var output = [];
+//   for (var n=1; n <= input; n++) {
+//     output.push(n);
+//   };
+//   return output;
+//
+// };
+//
+
+// // var replaceFive = function(input) {
+// //   // var pingpong = "";
+// //   for(var n = 0; n <= input; n++) {
+// //   if(n % 5 === 0);
+// //   };
+// //   return "pong";
+// // };
+// //
+// // var replaceFifteen = function(input) {
+// //   // var pingpong = "";
+// //   for(var n = 0; n <= input; n++) {
+// //   if(n % 15 === 0);
+// //   };
+// //   return "pingpong";
+// // };
+//
+// console.log()
+// $(document).ready(function() {
+//   $("form#pingpong").submit(function() {
+//     // var countTo = parseInt($("input#countTo").val());
+//     debugger;
+//
+//   });
+// });
+// //CHECK count-up FOR CODE TO TURN ARRAY INTO <ul>
